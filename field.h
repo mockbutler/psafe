28bcb253a8a2b18b6316f1251dc786c491e33473
Marc Butler <mockbutler@gmail.com>
2022-05-08 16:07:28 +1000

    Restructure code into separate files
    Fix valgrind leak check script and rename to memcheck.bash
    Add valgrind profiling script profile.bash

--- field.h
+++ field.h
@@ -1,40 +1,39 @@
 #pragma once
 
-#include "ioutil.h"
-
 #include <crypto++/secblock.h>
 
+#include "ioutil.h"
+
 namespace psafe {
-	class Field {
-	protected:
-		CryptoPP::SecBlock<byte> block_;
-		uint32_t length_;
-		uint8_t tag_;
-	public:
-		Field(const CryptoPP::SecBlock<byte>& block) :
-			block_(block),
-			length_(Le32(&block[0])),
-			tag_(block[4])
-		{}
+class Field {
+protected:
+  CryptoPP::SecBlock<byte> block_;
+  uint32_t length_;
+  uint8_t tag_;
 
-		Field(const Field& field) :
-			block_(field.block_),
-			length_(field.length_),
-			tag_(field.tag_)
-		{}
+public:
+  Field(const CryptoPP::SecBlock<byte> &block)
+      : block_(block), length_(Le32(&block[0])), tag_(block[4]) {}
 
-		Field(Field&& field) {
-			block_.swap(field.block_);
-			length_ = field.length_;
-			tag_ = field.tag_;
-		}
+  Field(CryptoPP::SecBlock<byte> &&block)
+      : block_(block), length_(Le32(&block[0])), tag_(block[4]) {}
 
-		uint32_t Length() const { return length_; }
+  Field(const Field &field)
+      : block_(field.block_), length_(field.length_), tag_(field.tag_) {}
 
-		uint8_t Tag() const { return tag_; }
+  Field(Field &&field) {
+    block_.swap(field.block_);
+    length_ = field.length_;
+    tag_ = field.tag_;
+  }
 
-		std::string Text() const {
-			return std::string(reinterpret_cast<const char*>(block_.begin() + 5), length_);
-		}
-	};
-}
+  uint32_t Length() const { return length_; }
+
+  uint8_t Tag() const { return tag_; }
+
+  std::string Text() const {
+    return std::string(reinterpret_cast<const char *>(block_.begin() + 5),
+                       length_);
+  }
+};
+} // namespace psafe
